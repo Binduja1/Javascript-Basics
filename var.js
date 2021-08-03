9b0f3b5286baa792ed72a805edaabf77ef7419a4
Binduja1 <72519535+Binduja1@users.noreply.github.com>
2021-08-03 09:31:44 +0530

    variable programs

--- var.js
+++ var.js
@@ -1,11 +1,11 @@
 let y = "hi";
 let Y = 5;
 
-console.log(y); // hi
-console.log(Y); // 5
+console.log(a); // hi
+console.log(b); // 5
 
 //Javascript is case sensitive.
 
 //We can use var and let to declare the variables.
 let x;
-let y1;
+let y6;
